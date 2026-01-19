6c965c0725c83943a3b204e79b48d2a40b78bfc6
ZiadWaleed2003 <ziadwaleedmohamed2003@gmail.com>
2026-01-19 03:41:34 +0200

    feat (agent endpoint ) : added the endpoint for the agent and tested it out it's working babyyy

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 
 
 from backend.src.core.database import get_db_base , get_db , engine
-from backend.src.api.v1 import assets
+from backend.src.api.v1 import assets , chat
 
 Base = get_db_base()
 
@@ -11,7 +11,13 @@ Base.metadata.create_all(bind=engine)
 app = FastAPI(title="THE Ledger API")
 
 app.include_router(router= assets.router , prefix="/assets", tags=["Assets"])
+app.include_router(router=chat.router, prefix="/agent" , tags=["Agent"])
+
 
 @app.get("/health")
 def health_check():
     return {"status" : "It's working broski dw"}
+
+@app.get("/")
+def main():
+    return {"status" : "up and running twin"}
