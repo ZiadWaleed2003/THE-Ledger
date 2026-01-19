b38c408e46f91c4e34b0efa2cc2730a02f2ae952
ZiadWaleed2003 <ziadwaleedmohamed2003@gmail.com>
2026-01-19 03:45:58 +0200

    feat (CORS) : adding CORS rules for the API

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 
 from backend.src.core.database import get_db_base , get_db , engine
 from backend.src.api.v1 import assets , chat
@@ -9,6 +9,19 @@ Base = get_db_base()
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="THE Ledger API")
+
+origins = [
+    "http://localhost",
+    "http://localhost:8000",
+    "http://localhost:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router= assets.router , prefix="/assets", tags=["Assets"])
 app.include_router(router=chat.router, prefix="/agent" , tags=["Agent"])
